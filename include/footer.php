7a4ef8536c8ef7f3ffc16adcf4ddeec92d554cb7
tahir-official <tahirk.official@gmail.com>
2022-09-10 20:19:36 +0530

    main 100922 footer

--- include/footer.php
+++ include/footer.php
@@ -3,39 +3,34 @@
       <div class="row mb-5">
         <div class="col-lg-3 py-3">
         <a href="<?=$site_url?>" class="scrollto"><img style="width:150px" src="<?=$portal_detail->LOGO?>" alt="<?=$portal_detail->PROJECT?>" class="img-fluid"></a>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero amet, repellendus eius blanditiis in iusto eligendi iure.</p>
+          <p><?=$portal_detail->FOOTER_ONE?></p>
 
-          <div class="social-media-button">
-            <a href="#"><span class="mai-logo-facebook-f"></span></a>
-            <a href="#"><span class="mai-logo-twitter"></span></a>
-            <a href="#"><span class="mai-logo-google-plus-g"></span></a>
-            <a href="#"><span class="mai-logo-instagram"></span></a>
-            <a href="#"><span class="mai-logo-youtube"></span></a>
-          </div>
+          
         </div>
         <div class="col-lg-3 py-3">
           <h5>Company</h5>
           <ul class="footer-menu">
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Career</a></li>
-            <li><a href="#">Advertise</a></li>
+            <li><a href="about.php">About Us</a></li>
+            <li><a href="service.php">Services</a></li>
+            <li><a href="blog.php">Blog</a></li>
+            <li><a href="contact.php">Contact</a></li>
             <li><a href="terms_of_service.php">Terms of Service</a></li>
             <li><a href="privacy_policy.php">Privacy Policy</a></li>
           </ul>
         </div>
         <div class="col-lg-3 py-3">
           <h5>Contact Us</h5>
-          <p>203 Fake St. Mountain View, San Francisco, California, USA</p>
-          <a href="#" class="footer-link">+00 1122 3344 5566</a>
-          <a href="#" class="footer-link">seogram@temporary.com</a>
+          <a href="tel:<?=$portal_detail->SUPPORT_NUMBER?>" class="footer-link"><?=$portal_detail->SUPPORT_NUMBER?></a><br>
+          <a href="mailto:<?=$portal_detail->SUPPORT_EMAIL?>" class="footer-link"><?=$portal_detail->SUPPORT_EMAIL?></a>
         </div>
         <div class="col-lg-3 py-3">
-          <h5>Newsletter</h5>
-          <p>Get updates, news or events on your mail.</p>
-          <form action="#">
-            <input type="text" class="form-control" placeholder="Enter your email..">
-            <button type="submit" class="btn btn-success btn-block mt-2">Subscribe</button>
-          </form>
+          <h5>Social</h5>
+          <div class="social-media-button">
+            <a href="<?=$portal_detail->FACEBOOK?>"><span class="mai-logo-facebook-f"></span></a>
+            <a href="<?=$portal_detail->TWITTER?>"><span class="mai-logo-twitter"></span></a>
+            <a href="<?=$portal_detail->INSTAGRAM?>"><span class="mai-logo-instagram"></span></a>
+            <a href="<?=$portal_detail->YOUTUBE?>"><span class="mai-logo-youtube"></span></a>
+          </div>
         </div>
       </div>
 
