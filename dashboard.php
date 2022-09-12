62691d146c0249e53ed3939b5cfe0fa59eb697d1
tahir-official <tahirk.official@gmail.com>
2022-09-13 05:07:21 +0530

    main 05 130922 dashboard setup

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,6 @@
 <?php
    include_once('include/header.php');
+   include ("include/redirectcondtion.php");
          
 ?>
 <div class="container">
@@ -9,17 +10,60 @@
             <nav aria-label="Breadcrumb">
                <ul class="breadcrumb justify-content-center py-0 bg-transparent">
                   <li class="breadcrumb-item"><a href="<?=$site_url?>">Home</a></li>
-                  <li class="breadcrumb-item active">Login</li>
+                  <li class="breadcrumb-item active">Dashboard</li>
                </ul>
             </nav>
-            <h1 class="text-center">Login</h1>
+            <h1 class="text-center">Dashboard</h1>
          </div>
       </div>
    </div>
 </div>
 </header>
-
+<!--dashboard-->
+<div class="page-section">
+   <div class="container">
+      <div id="pagelayout_area">
+         <section class="site-maincontentarea fullwidth">
+            <div class="entry-content">
+               <!--filter-->
+               <div class="filteruus main_page">
+                  <div class="row">
+                     <div class="col-sm-4">
+                        <?php
+                           include ("include/dashboard-sidebar.php");
+                        ?>
+                     </div>
+                     <div class="col-sm-8">
+                        <h2 class="h2_hedddin">Profile</h2>
+                        <div class="rightt">
+                           <form method="post" action="http://localhost/wordpress/wedding/wp-admin/admin-post.php">
+                              <div class="form-group">
+                                 <label>Name</label>
+                                 <input type="text" name="username" id="username" placeholder="Your Name" class="form-control" value="admin" required="">
+                              </div>
+                              <div class="form-group">
+                                 <label>Phone</label>
+                                 <input name="phone" id="phone" placeholder="Phone" class="form-control" type="text" required="" value="">
+                              </div>
+                              <div class="form-group">
+                                 <label>Email</label>
+                                 <input name="email" id="email" placeholder="Email" class="form-control" type="email" readonly="" value="tahirk.official@gmail.com">
+                              </div>
+                              <div class="form-group">
+                                 <input type="hidden" name="action" value="user_profile_update_act">
+                                 <button class="btn btn_theme btn-lg" type="submit">Update</button>
+                              </div>
+                           </form>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </section>
+      </div>
+   </div>
+</div>
 <?php
-   print_r($_SESSION);
+   //print_r($_SESSION);
    include_once('include/footer.php');
 ?>
---
+++ include/dashboard-sidebar.php
@@ -0,0 +1,16 @@
+<div class="left_side">
+    <div class="profile_box1" style="background: url(assets/img/dashboard.png);background-repeat: no-repeat;background-position: center;background-size: cover;">
+        <div class="profile_box2" data-toggle="modal" data-target="#upload_img">
+            <img src="">
+        </div>
+        <h4>admin</h4>
+        
+    </div>
+    <ul class="ul_set sider_barr">
+        <li class="active"><a href="http://localhost/wordpress/wedding/dashboard/">Profile</a></li>
+        <li><a href="http://localhost/wordpress/wedding/inbox/"><i class="fa fa-comments-o" aria-hidden="true"></i> Inbox <span class="notification_count" id="notification_count">0</span></a> </li>
+        <li><a href="http://localhost/wordpress/wedding/favorite-vendor/"><i class="fa fa-users" aria-hidden="true"></i> Favorite Vendor</a></li>
+        <li><a href="http://localhost/wordpress/wedding/change-password/"><i class="fa fa-unlock-alt" aria-hidden="true"></i>Change Password</a></li>
+        <li><a href="http://localhost/wordpress/wedding/wp-login.php?action=logout&amp;_wpnonce=60c0475b87"><i class="fa fa-sign-out" aria-hidden="true"></i>Log Out</a></li>
+    </ul>
+</div>
